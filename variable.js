5bea761942e3845da566b46b117c780b2a4562e4
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-11 11:32:32 +0530

    declared a variable.js file and and checked the type of all data type

---
+++ variable.js
@@ -0,0 +1,25 @@
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+// 1. String
+let fullName = "Ved Dadhich";
+console.log(fullName);
+console.log(typeof fullName);
+
+// 2. Number
+let phoneNumber = 632599874;
+console.log(phoneNumber);
+console.log(typeof phoneNumber);
+
+// 3. Boolean
+let isLoggedFromFB = true;
+console.log(isLoggedFromFB);
+console.log(typeof isLoggedFromFB);
+
+// 4. Undefined
+let firstName;
+console.log(typeof firstName);
+
+// 5. Null
+let a = null;
+console.log(a);
+console.log(typeof a);
