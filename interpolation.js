21661884feb6b37798c0fd3553a6caf048c32e45
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-11 11:44:43 +0530

    used interpolation to show data

---
+++ interpolation.js
@@ -0,0 +1,15 @@
+// 2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+let firstName = "Ved";
+let lastName = "Dadhich";
+let maritalStatus = "unmarried";
+let country = "India";
+let age = 26;
+
+console.log(`
+    My FirstName is: ${firstName}
+    My LastName is: ${lastName}
+    Marital Status is: ${maritalStatus}
+    My Country is: ${country}
+    My Age is: ${age}
+`);
