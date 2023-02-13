6af506b704c08d396913e9ec852dd0244016bde9
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-13 11:46:54 +0530

    solved question through lastIndexOf method

---
+++ lastIndexOf.js
@@ -0,0 +1,4 @@
+// 7. Use lastIndexOf to determine the position of the last occurrence of a script.
+var names = [ 'raj', 'cse', 'sachin', 'portal' ];
+var a = names.lastIndexOf("sachin");
+console.log(a);
