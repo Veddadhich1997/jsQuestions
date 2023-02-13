8bc82932e7b06cba4758a35d4e0fb88ad8bde587
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-13 19:39:56 +0530

    got slope between 2 points

---
+++ slope.js
@@ -0,0 +1,12 @@
+// 14. Slope is m = (y2-y1)/(x2-x1). Find the slope between point (2, 2) and point(6,10)
+
+var x1 = 2;
+var y1 = 2;
+var x2 = 6;
+var y2 = 10;
+
+var m = (y2-y1)/(x2-x1);
+
+console.log(m);
+
+// slope will be 2
