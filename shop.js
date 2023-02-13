7613f36d2679438d4ce55f77e1ae001cdaa9ad76
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-14 01:45:02 +0530

    modified array using different methods

---
+++ shop.js
@@ -0,0 +1,23 @@
+// 25. In the following shopping cart add, remove, edit items
+//     => const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+//     - add 'Meat' in the beginning of your shopping cart if it has not been already added
+//     - add Sugar at the end of you shopping cart if it has not been already added
+//     - remove 'Honey' if you are allergic to honey
+//     - modify Tea to 'Green Tea'
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+//     - add 'Meat' in the beginning of your shopping cart if it has not been already added
+shoppingCart.unshift("Meat");
+console.log(shoppingCart);
+
+//     - add Sugar at the end of you shopping cart if it has not been already added
+shoppingCart.push("Sugar");
+console.log(shoppingCart);
+
+//     - remove 'Honey' if you are allergic to honey
+shoppingCart.splice(4,1);
+console.log(shoppingCart);
+
+//     - modify Tea to 'Green Tea'
+shoppingCart[3] = "Green Tea";
+console.log(shoppingCart);
