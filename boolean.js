dc89eec29097bafc11261f4b8328c2b9f32ca4a7
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-13 16:16:21 +0530

    answer for truthy and falsy values

---
+++ boolean.js
@@ -0,0 +1,24 @@
+// 10. Boolean value is either true or false.
+//     - Write three JavaScript statement which provide truthy value.
+//     - Write three JavaScript statement which provide falsy value.
+
+// falsy
+var a = 0;
+console.log(a);
+
+var a =NaN;
+console.log(a);
+
+var a = null;
+console.log(a);
+
+
+// truthy
+var a = -1;
+console.log(a);
+
+var b = 1;
+console.log(b);
+
+var c = "0";
+console.log(c);
