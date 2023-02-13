6c012c669b6c1b7c26700dcf83bdd93eed996305
Ved Dadhich <dadhichved1997@gmail.com>
2023-02-13 16:43:47 +0530

    solved with logical operator

---
+++ logical.js
@@ -0,0 +1,83 @@
+// 11. Figure out the result of the following comparison expression first without using console.log(). After you decide the result confirm it using console.log()
+//     - 4 > 3
+//     - 4 >= 3
+//     - 4 < 3
+//     - 4 <= 3
+//     - 4 == 4
+//     - 4 === 4
+//     - 4 != 4
+//     - 4 !== 4
+//     - 4 != '4'
+//     - 4 == '4'
+//     - 4 === '4'
+//     - Find the length of python and jargon and make a falsy comparison statement.
+
+// - 4 > 3
+// 4 is greater than 3 = true
+
+
+
+// - 4 >= 3
+// 4 is greater than equal to 3 = true
+
+
+// - 4 < 3
+// 4 is less than three = false
+
+
+// - 4 <= 3
+// 4 is less than equal to three = false
+
+
+// - 4 == 4
+// 4 is equal to equal to 4 = true
+
+
+// - 4 === 4
+// 4 is equal to equal to 4 and data type is also same = true
+
+
+// - 4 != 4
+// 4 is not equal to 4 = false
+
+
+// - 4 !== 4
+// 4 is not equal to equal to 4 = false
+
+// - 4 != '4'
+// 4 is not equal to '4' =false
+
+
+// - 4 == '4'
+// 4 is equal to equal to '4' = true
+
+
+// - 4 === '4'
+// 4 is equal to equal to '4' = false
+
+
+// - Find the length of python and jargon and make a falsy comparison statement.
+let a = 'python';
+let b = 'jargon';
+
+if(a.length==b.length){
+    console.log("length of a is equal to length of b");
+}
+
+else{
+    console.log("this statement is false");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
